9e8c8949b32a2217c46d8c1533a4a8f982dd20fa
github username <github email address>
2017-05-18 10:23:24 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,67 @@
+
+require 'pry'
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+
+    @artist = artist
+    @@artists << self.artist
+
+    @genre = genre
+    @@genres << self.genre
+
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count.has_key?(genre)
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 0
+        genre_count[genre] += 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count.has_key?(artist)
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 0
+        artist_count[artist] += 1
+      end
+    end
+    artist_count
+  end
+
+end
+
+a = Song.new("hotline", "drake", "r&b")
+b = Song.new("wheel", "hootie", "country")
+Song.genre_count
+
+# binding.pry
